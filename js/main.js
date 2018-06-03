1e6078bbaa686ed00e94edff0304f7451714413d
clint <clint.cho@kakaocorp.com>
2018-06-03 23:35:17 +0900

    Add: google analytics js code

--- js/main.js
+++ js/main.js
@@ -252,7 +252,7 @@
 	
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Dec 02, 2017 15:37:25").getTime();
+		var countDownDate = new Date("Sep 01, 2018 17:00:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
